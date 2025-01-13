01dc2d5bc039dc6a918988c8bd5261967fa2240e
sousuke71 <164746474+sousuke71@users.noreply.github.com>
2025-01-13 14:05:41 +0900

    Create 電卓.svelte

---
+++ 電卓.svelte
@@ -0,0 +1,27 @@
+<script>
+  let val1 = 0;
+  let val2 = 0;
+</script>
+
+<input type="number" bind:value={val1}>
+<input type="number" bind:value={val2}>
+
+<p>{val1 + val2}</p>
+<template>
+  <div>
+    <input type="number" v-model.number="val1">
+    <input type="number" v-model.number="val2">
+    <p>{{val1 + val2}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        val1: 0,
+        val2: 0,
+      };
+    }
+  };
+</script>
